27a01e8dab0fcf93a89364cf56f31b4c90bd80f6
Patrick Pin <patrickpin840@hotmail.com>
2020-01-28 23:23:41 -0500

    [R2, R3] Implement kinematic flee, arrive and wander, frozen tag and color

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// implement AI movement and animate NPCs
 public class Player : MonoBehaviour
 {
     Animator animator;
+    
+    public Vector3 destination;
+    public float distance;
+    public float speed;
+    public float maxSpeed;
+    public float stopRadius;
+    public float slowRadius;
+    public float rotationDegreesPerSecond;
 
-	public float maxVelocity;
-    public float acceleration;
-	public float deccelerationMultiplier;
-	public float rotationDegreesPerSecond = 360;
-	float velocity;
-
-	Vector2 input = Vector2.zero;
-    public bool tagged = false;
-    public bool target = false;
+    public bool arrive;
+    public bool flee;
 
     void Start()
     {
@@ -23,45 +23,9 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        float horizontal = Input.GetAxis ("Horizontal");
-        float vertical = Input.GetAxis ("Vertical");
-        
-		input.x = horizontal;
-		input.y = vertical;
+        animator.SetFloat ("Blend", speed / maxSpeed);
 
-		float inputMag = input.magnitude;
-
-		if (!Mathf.Approximately (vertical, 0.0f) || !Mathf.Approximately (horizontal, 0.0f)) {
-			Vector3 direction = new Vector3 (horizontal, 0.0f, vertical);
-			direction = Vector3.ClampMagnitude (direction, 1.0f);
-
-			if (velocity < maxVelocity) {
-				velocity += acceleration * Time.deltaTime;
-				if (velocity > maxVelocity)
-					velocity = maxVelocity;
-			}
-
-			transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.LookRotation (direction), rotationDegreesPerSecond * Time.deltaTime);
-            
-		} else if (velocity > 0){
-			
-			velocity -= acceleration * deccelerationMultiplier * Time.deltaTime;
-			if (velocity < 0)
-				velocity = 0;
-		}
-
-        if (transform.position.y < 0.1)
-        {
-            transform.position += transform.forward * Time.deltaTime * velocity;
-        }
-        else
-        {
-            Vector3 newPos = new Vector3(transform.position.x, 0, transform.position.z);
-            transform.position = newPos;
-        }
-
-		animator.SetFloat ("Blend", velocity / maxVelocity);
-
+        //Appear on other side of arena when crossing a boundary
         if (transform.position.z > 50)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -50);
@@ -79,27 +43,132 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(50, transform.position.y, transform.position.z);
         }
 
-        if (tagged)
+        // Set target and change character colour based on current tag
+        if (this.tag == "Tagged")
         {
-            this.tag = "Tagged";
+            Target("Not Tagged");
+            maxSpeed = 3;
             transform.GetChild(0).gameObject.SetActive(false);
             transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(2).gameObject.SetActive(false);
+        }
+        else if (this.tag == "Frozen")
+        {
+            speed = 0;
+            arrive = false;
+            flee = false;
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(false);
+            transform.GetChild(2).gameObject.SetActive(true);
         }
         else
         {
-            this.tag = "Not Tagged";
+            Target("Frozen");
+            maxSpeed = 1.5f;
             transform.GetChild(0).gameObject.SetActive(true);
             transform.GetChild(1).gameObject.SetActive(false);
-            velocity = 0;
+            transform.GetChild(2).gameObject.SetActive(false);
+        }
+
+        if (arrive)
+        {
+            Vector3 Direction = destination - this.transform.position;
+            Vector3 perp = Vector3.Cross(transform.forward, Direction);
+            float CurrentRotation = Vector3.Dot(perp, Vector3.up);
+            Kinematic(Direction, CurrentRotation);
+            speed = Mathf.Clamp(speed, 0, maxSpeed);
+            this.transform.Translate(Vector3.forward * Time.deltaTime * 0.5f * 1.75f * speed);
+        }
+
+        if (flee)
+        {
+            Vector3 direction = transform.position - destination;
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), rotationDegreesPerSecond * Time.deltaTime);
+
+            Vector3 perp = Vector3.Cross(transform.forward, direction);
+            float CurrentRotation = Vector3.Dot(perp, Vector3.up);
+            Kinematic(direction, CurrentRotation);
+            this.transform.Translate(Vector3.forward * Time.deltaTime * 0.5f * 1.105f * speed);
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    protected void Kinematic(Vector3 Direction, float CurrentRotation)
     {
-        if (other.tag == "Not Tagged")
+        if (speed < maxSpeed)
         {
-            tagged = false;
-            other.GetComponent<Player>().tagged = true;
+            if (distance <= stopRadius)
+            {
+                this.transform.Rotate(Vector3.up, CurrentRotation);
+                speed = maxSpeed;
+            }
+            else if (distance < slowRadius)
+            {
+                speed = 0;
+                if (CurrentRotation > 1)
+                    this.transform.Rotate(Vector3.up, 1);
+                else if (CurrentRotation < -1)
+                    this.transform.Rotate(Vector3.up, -1);
+                else
+                    speed = maxSpeed;
+            }
+        }
+        else
+        {
+            if (Vector3.Angle(Direction, transform.forward) <= 30)
+            {
+                if (CurrentRotation > 0)
+                    this.transform.Rotate(Vector3.up, 1);
+                else if (CurrentRotation < 0)
+                    this.transform.Rotate(Vector3.up, -1);
+            }
+            else
+            {
+                speed = 0f;
+            }
+        }
+    }
+
+    // Find target depending on tag
+    void Target(string target)
+    {
+        // Use kinematic arrive for chosen target with appropriate tag
+        foreach (GameObject opponent in GameObject.FindGameObjectsWithTag(target))
+        {
+            if (Vector3.Distance(transform.position, opponent.transform.position) < slowRadius)
+            {
+                distance = Vector3.Distance(transform.position, opponent.transform.position);
+                destination = opponent.transform.position;
+                arrive = true;
+                flee = false;
+            }
+        }
+
+        // Flee from tagged character if they are close enough
+        if (this.tag == "Not Tagged")
+        {
+            GameObject taggedOpponent = GameObject.FindGameObjectWithTag("Tagged");
+            if (Vector3.Distance(transform.position, taggedOpponent.transform.position) < slowRadius)
+            {
+                distance = Vector3.Distance(transform.position, taggedOpponent.transform.position);
+                destination = taggedOpponent.transform.position;
+                flee = true;
+                arrive = false;
+            }
+        }
+    }
+
+    // Change tags on collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Not Tagged" && this.tag == "Tagged")
+        {
+            Debug.Log("frozen!");
+            collision.gameObject.tag = "Frozen";
+        }
+        if (collision.gameObject.tag == "Frozen" && this.tag == "Not Tagged")
+        {
+            Debug.Log("saved!");
+            collision.gameObject.tag = "Not Tagged";
         }
     }
 }
